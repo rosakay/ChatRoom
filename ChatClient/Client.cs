1ac583c1fb6ab8a70ff67472a8aa8261b53ecc51
STUDENT-PC\Student <rosakay11@gmail.com>
2022-06-16 20:52:29 +0800

    feat: MultiClients, Library

---
+++ ChatClient/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("====================================");
+            var client = new ChatLibrary.ChatClient();
+
+            var succeed = client.Connect("127.0.0.1", 4099);
+
+            if (!succeed)
+            {
+                return;
+            }
+
+            Console.WriteLine("<You can press any key to start entering text...>");
+
+            while (true)
+            {
+                var msg = Console.ReadLine();
+
+                if (msg.ToLower() == "exit")
+                {
+                    Console.WriteLine("<Bye...>");
+                    client.Disconnect();
+                    break;
+                }
+
+                client.SendData(msg);
+                Console.WriteLine("<Message sent, press any key to start entering text again...>");
+            }
+        }
+    }
+}
---
+++ ChatServer/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    internal class Server
+    {
+        static HashSet<TcpClient> clients = new HashSet<TcpClient>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("====================================");
+            var server = new ChatLibrary.ChatServer();
+            server.Bind(4099);
+            server.Start();
+        }
+    }
+}
