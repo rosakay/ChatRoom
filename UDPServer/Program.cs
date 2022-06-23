236d76dd5fd0ee6f32f8077dff0ebaac5cf4a515
STUDENT-PC\Student <rosakay11@gmail.com>
2022-06-23 20:08:45 +0800

    Chore: UDP Chat

---
+++ UDPServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UDPServer
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("====================================");
+      var server = new ChatServer();
+      server.Bind(4099);
+      server.Run();
+    }
+  }
+}
