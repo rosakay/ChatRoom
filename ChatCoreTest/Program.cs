65fb30d60efa5027c4ae5112b0d2ec44ce9b5978
onekwei <rosakay11@gmail.com>
2022-06-27 00:32:07 +0800

    Feat: HomeWork 1

--- ChatCoreTest/Program.cs
+++ ChatCoreTest/Program.cs
@@ -4,73 +4,97 @@ using Newtonsoft.Json;
 
 namespace ChatCoreTest
 {
-  internal class Program
-  {
-    private static byte[] m_PacketData;
-    private static uint m_Pos;
-
-    public static void Main(string[] args)
+    internal class Program
     {
-      m_PacketData = new byte[1024];
-      m_Pos = 0;
+        private static byte[] m_PacketData;
+        private static byte[] m_PacketDataNoReverse;
+        private static uint m_Pos;
+        private static uint r_Pos;
 
-      Write(109);
-      Write(109.99f);
-      Write("Hello!");
+        public static void Main(string[] args)
+        {
+            m_PacketDataNoReverse = new byte[1024];
+            m_PacketData = new byte[1024];
+            m_Pos = 0;
 
-      Console.Write($"Output Byte array(length:{m_Pos}): ");
-      for (var i = 0; i < m_Pos; i++)
-      {
-        Console.Write(m_PacketData[i] + ", ");
-      }
-      Console.ReadLine();
+            r_Pos = 0;
+
+            Write(109);
+            Write(109.99f);
+            Write("Hello!");
+
+            //Console.Write($"Output Byte array(length:{m_Pos}): ");
+
+            for (var i = 0; i < m_Pos; i++)
+            {
+                Console.Write(m_PacketData[i] + ", ");
+            }
+            _Read(m_PacketDataNoReverse);
+
+            Console.ReadLine();
+        }
+
+        // write an integer into a byte array
+        private static bool Write(int i)        //寫入整數
+        {
+            // convert int to byte array
+            var bytes = BitConverter.GetBytes(i); // 取得 i 位元組序列，並賦值於 bytes
+            _Write(bytes);
+            return true;
+        }
+
+        // write a float into a byte array
+        private static bool Write(float f)      //寫入浮點數
+        {
+            // convert int to byte array
+            var bytes = BitConverter.GetBytes(f); // 取得 f 位元組序列，並賦值於 bytes
+            _Write(bytes);
+            return true;
+        }
+
+        // write a string into a byte array
+        private static bool Write(string s)     //寫入字串
+        {
+            // convert string to byte array
+            var bytes = Encoding.Unicode.GetBytes(s);     //先將字串編碼成位元組序列，並賦值於 bytes
+
+            // write byte array length to packet's byte array
+            if (Write(bytes.Length) == false)   // 若目前bytes陣列沒Data
+            {
+                return false;   // 回傳false
+            }
+
+            _Write(bytes);   // 有Data的話，將Data複製(寫入)到指定的一維陣列(m_PacketData)
+            return true;
+        }
+
+        // write a byte array into packet's byte array
+        private static void _Write(byte[] byteData)     //將 bytes 資料陣列複製到指定的一維陣列
+        {
+            // converter little-endian to network's big-endian
+            if (BitConverter.IsLittleEndian)    //如果byte順序是屬於LittleEndian格式
+            {
+                byteData.CopyTo(m_PacketDataNoReverse, m_Pos);  //存取無顛倒版本
+                Array.Reverse(byteData);    //則將順序顛倒
+            }
+
+            byteData.CopyTo(m_PacketData, m_Pos);   // 將 Data 於 m_Pos 位置開始複製(寫入)到指定的一維陣列(m_PacketData)
+            m_Pos += (uint)byteData.Length;    // 將有無資料的分界位置加上並移動為剛剛加入的資料長度
+        }
+
+
+        private static void _Read(byte[] packetData)     //將 m_PacketData 資料陣列，重新編譯成可視讀之文字
+        {
+            string[] pd = new String[3];
+            Console.WriteLine("");
+
+            //string[] pd = packetData;
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"整數:{BitConverter.ToInt32(packetData, 0)}");     //編譯成整數
+            Console.WriteLine($"浮點數:{BitConverter.ToSingle(packetData, 4)}");   //編譯成浮點數
+            Console.WriteLine($"字串:{Encoding.Unicode.GetString(packetData, 12, packetData.Length - 12)}");  //編譯成字串
+        }
     }
-
-    // write an integer into a byte array
-    private static bool Write(int i)
-    {
-      // convert int to byte array
-      var bytes = BitConverter.GetBytes(i);
-      _Write(bytes);
-      return true;
-    }
-
-    // write a float into a byte array
-    private static bool Write(float f)
-    {
-      // convert int to byte array
-      var bytes = BitConverter.GetBytes(f);
-      _Write(bytes);
-      return true;
-    }
-
-    // write a string into a byte array
-    private static bool Write(string s)
-    {
-      // convert string to byte array
-      var bytes = Encoding.Unicode.GetBytes(s);
-
-      // write byte array length to packet's byte array
-      if (Write(bytes.Length) == false)
-      {
-        return false;
-      }
-
-      _Write(bytes);
-      return true;
-    }
-
-    // write a byte array into packet's byte array
-    private static void _Write(byte[] byteData)
-    {
-      // converter little-endian to network's big-endian
-      if (BitConverter.IsLittleEndian)
-      {
-        Array.Reverse(byteData);
-      }
-
-      byteData.CopyTo(m_PacketData, m_Pos);
-      m_Pos += (uint)byteData.Length;
-    }
-  }
 }
