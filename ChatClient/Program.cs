56988f05bfeab3d825d110ab8a905da909e6fa54
STUDENT-PC\Student <rosakay11@gmail.com>
2022-06-28 20:38:18 +0800

    chore: bye()

---
+++ ChatClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ChatClient
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("====================================");
+      var client = new ChatCore.ChatClient();
+
+      Console.WriteLine( "<Please enter your name...>" );
+      var name = Console.ReadLine();
+      client.SetName( name );
+
+      var succeed = client.Connect("127.0.0.1", 4099);
+      if (!succeed)
+      {
+        return;
+      }
+
+      Console.WriteLine("<You can press any key to start entering text...>");
+
+      while (true)
+      {
+        while( Console.KeyAvailable == false )
+        {
+          client.Refresh();
+          System.Threading.Thread.Sleep( 1 );
+        }
+
+        var msg = Console.ReadLine();
+
+        if (msg == "exit")
+        {
+          Console.WriteLine("<Bye...>");
+          client.Bye();
+          break;
+        }
+
+        client.SendMessage(msg);
+        Console.WriteLine("<Message sent, press any key to start entering text again...>");
+      }
+    }
+  }
+}
